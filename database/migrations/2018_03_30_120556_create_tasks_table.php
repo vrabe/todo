6b1474a3f54fcc577338727bb2f2c6f1223782ed
vrabe <git@vrabe.tw>
2018-03-31 00:19:02 +0800

    tasks table fix

--- database/migrations/2018_03_30_120556_create_tasks_table.php
+++ database/migrations/2018_03_30_120556_create_tasks_table.php
@@ -18,13 +18,13 @@ class CreateTasksTable extends Migration
             $table->timestamps();
             $table->integer('project_id')->unsigned();
             $table->integer('category_id')->unsigned();
-            $table->integer('description_id')->unsigned();
-            $table->integer('time_needed')->unsigned();
+            $table->integer('description_id')->unsigned()->nullable();
+            $table->integer('time_needed')->unsigned()->nullable();
             $table->string('priority', 32);
             $table->string('severity', 32);
             $table->string('status', 32);
             $table->string('summary', 128);
-            $table->timestamp('due_date');
+            $table->timestamp('due_date')->nullable();
         });
     }
 
