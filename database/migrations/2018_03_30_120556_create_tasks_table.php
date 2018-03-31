53594224f90be49b3dd47042afa35aaa5885270e
vrabe <git@vrabe.tw>
2018-04-01 00:08:59 +0800

    task descriptions table fix 3

--- database/migrations/2018_03_30_120556_create_tasks_table.php
+++ database/migrations/2018_03_30_120556_create_tasks_table.php
@@ -18,7 +18,7 @@ class CreateTasksTable extends Migration
             $table->timestamps();
             $table->integer('project_id')->unsigned();
             $table->integer('category_id')->unsigned();
-            $table->integer('description_id')->unsigned()->nullable();
+            $table->integer('description_id')->unsigned()->nullable()->unique();
             $table->integer('time_needed')->unsigned()->nullable();
             $table->string('priority', 32);
             $table->string('status', 32);
