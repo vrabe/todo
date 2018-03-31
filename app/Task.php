ba771b8703d8effbe6961c9ee15a4f6eb4c9f6e3
vrabe <git@vrabe.tw>
2018-03-31 23:59:53 +0800

    task descriptions table fix 2

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+    * Get the description record associated with the task.
+    */
+    public function description()
+    {
+        return $this->hasOne('App\TaskDescription');
+    }
 }
