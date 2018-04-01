014012a1e73d55eb5f27512d136beb324a1914b9
vrabe <git@vrabe.tw>
2018-04-01 19:55:13 +0800

    task relationships table 1

--- database/migrations/2018_04_01_092949_create_task_relationships_table.php
+++ database/migrations/2018_04_01_092949_create_task_relationships_table.php
@@ -16,7 +16,19 @@ class CreateTaskRelationshipsTable extends Migration
         Schema::create('task_relationships', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->integer('source_task_id')->unsigned();
+            $table->integer('destination_task_id')->unsigned();
+            $table->smallInteger('relationship_type');
         });
+
+        Schema::table('task_relationships', function (Blueprint $table) {
+          $table->foreign('source_task_id')->references('id')->on('tasks')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+          $table->foreign('destination_task_id')->references('id')->on('tasks')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        }
     }
 
     /**
