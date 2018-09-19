2f67da5cba78497134a995bc74959c9ba6466dc1
vrabe <git@vrabe.tw>
2018-09-20 00:37:05 +0800

    TaskController ctor

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * The task repository instance.
+     *
+     * @var TaskRepository
+     */
+    protected $repository;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param  TaskRepository  $tasks
+     * @return void
+     */
+    public function __construct(TaskRepository $tasks)
+    {
+        $this->repository = $tasks;
+    }
+
 }
---
+++ tests/Unit/TaskControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Repositories\TaskRepository;
+use App\Models\Task;
+
+class TaskControllerTest extends TestCase
+{
+    /**
+     * @var TaskController
+     */
+    protected $controller = null;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->controller = new TaskController(new TaskRepository(new Task()));
+    }
+
+    public function tearDown()
+    {
+        $this->controller = null;
+    }
+}
