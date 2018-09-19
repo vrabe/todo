5d43f75329d014df78c6c2cfba244321347ce26e
vrabe <git@vrabe.tw>
2018-09-20 00:45:52 +0800

    leave a basic test

--- tests/Unit/TaskControllerTest.php
+++ tests/Unit/TaskControllerTest.php
@@ -24,4 +24,14 @@ class TaskControllerTest extends TestCase
     {
         $this->controller = null;
     }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
 }
