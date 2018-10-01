2e3cd7e22088a892d0a5a769c8dbe092e0463357
vrabe <git@vrabe.tw>
2018-10-02 00:46:18 +0800

    remove oldSeedData

--- tests/Unit/TaskRepositoryTest.php
+++ tests/Unit/TaskRepositoryTest.php
@@ -19,39 +19,6 @@ class TaskRepositoryTest extends TestCase
     protected static $isSetUp = false;
 
     /**
-     * add 100 seed tasks
-     */
-    protected function oldSeedData()
-    {
-        $priority = ['low', 'medium', 'high'];
-        $status = ['new', 'finished'];
-        for ($i = 1; $i <= 10; $i ++) {
-            $projectEntry = new Project();
-            $projectEntry->name = 'Project ' . $i;
-            $projectEntry->status = $status[$i % 2];
-            $projectEntry->save();
-            $projectEntry = null;
-        }
-        for ($i = 1; $i <= 100; $i ++) {
-            $entry = new Task();
-            $entry->project_id = floor($i / 10);
-            $entry->time_needed = 60 * 60 * $i;
-            $entry->priority = $priority[$i % 3];
-            $entry->status = $status[$i % 2];
-            $entry->summary = str_random(128);
-            $entry->start_time = date("Y-m-d H:i:s");
-            $entry->due_time = date("Y-m-d H:i:s");
-            $entry->save();
-            $description = new TaskDescription();
-            $description->text = 'For task ' . $i;
-            $entry->description()->save($description);
-            $entry->save();
-            $entry = null;
-            $description = null;
-        }
-    }
-
-    /**
      * add seed tasks
      *
      * @param  int  $qty number of tasks
